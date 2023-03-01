02c66d0cc2c344b393070dbdb13989377d9d27d2
Vladyslaviuss <thoughts.stream@gmail.com>
2023-03-01 20:43:37 +0200

    :fire: Delete unused file

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config(object):
 
     API_HASH = str(os.environ.get("API_HASH", "0266b63175fe226f5b9caa12d9c91003"))
 
-    BOT_TOKEN = str(os.environ.get("BOT_TOKEN", "5963630341:AAHkLqm76c4Iqp-A3UrVSSdRN3FNw8V_x38"))
+    BOT_TOKEN = str(os.environ.get("BOT_TOKEN", "6046558159:AAHbVK5VsHI9j1gBzQ7OpB1QAEiROeqnQdo"))
     
     OWNER_ID = int(os.environ.get("OWNER_ID", 284134017))
 
@@ -14,9 +14,9 @@ class Config(object):
     
     DB_NAME = str(os.environ.get("DB_NAME", "vladyslavius"))
     
-    LOG_GROUP = int(os.environ.get("LOG_GROUP", "-882487827"))
+    # LOG_GROUP = int(os.environ.get("LOG_GROUP", "-859771439"))
 
-    # LOG_GROUP = int(os.environ.get("LOG_GROUP", "-805783554")) # Main group
+    LOG_GROUP = int(os.environ.get("LOG_GROUP", "-1001801500302")) # Main group
 
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 
